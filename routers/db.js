9265026b54d4853e487b7bc3c1c2902f2c159908
Karoline Spohn <118634774+karolinespohn@users.noreply.github.com>
2023-06-03 15:23:51 +0200

    db.js ssl connection

--- routers/db.js
+++ routers/db.js
@@ -1,3 +1,13 @@
 const pgp= require("pg-promise")()
+const connection = {
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    ssl: { rejectUnauthorized: false } // This line enables SSL
+};
+
+
 const db = pgp(process.env.DATABASE_URL);
 module.exports = db;
