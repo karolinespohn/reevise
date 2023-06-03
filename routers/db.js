eae5816e8ddf1ac37e0d52d00dd750108cf86d86
Karoline Spohn <118634774+karolinespohn@users.noreply.github.com>
2023-06-03 15:38:04 +0200

    test

--- routers/db.js
+++ routers/db.js
@@ -5,7 +5,7 @@ const connection = {
     database: process.env.DATABASE_NAME,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    ssl: { rejectUnauthorized: false } // This line enables SSL
+    // ssl: { rejectUnauthorized: false } // This line enables SSL
 };
 
 
